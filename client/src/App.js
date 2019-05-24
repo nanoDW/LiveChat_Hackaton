b53d3be8453f190a6cd0016528b25c4a6d01e8a8
Aaspyr <aaspyr@gmail.com>
2019-05-24 23:27:23 +0200

    MemeGenerator

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-
-=======
->>>>>>> ae418f5a3c709b8879e8e8722e19537ccbb3176e
 
 function App() {
   return (
---
+++ client/src/MemeGenerator.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import axios from 'axios';
+
+class MemeGenerator extends React.Component{
+
+}
+export default MemeGenerator;
