2b37b6b883c27a8cdf36c26ccd7f9a248cc8c698
Aaspyr <aaspyr@gmail.com>
2019-05-24 21:59:44 +0200

    .

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-<<<<<<< HEAD
-
-=======
->>>>>>> ae418f5a3c709b8879e8e8722e19537ccbb3176e
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
