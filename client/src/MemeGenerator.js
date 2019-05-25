042c70770fc247552a12f63c5b06404949bf588f
nanoDW <s.fluder@interia.eu>
2019-05-25 05:05:17 +0200

    Make some fixes

--- client/src/MemeGenerator.js
+++ client/src/MemeGenerator.js
@@ -3,45 +3,33 @@ import axios from 'axios';
 
 
 class MemeGenerator extends React.Component{
-<<<<<<< HEAD
-    state = {memes: []};
-=======
     state = {
         meme: [],
         isMounted: false
     };
 
     abortController = new AbortController();
->>>>>>> textarea
 
     componentDidMount(){
         this.setState({ isMounted: true});
         this.getMeme();
     }
-<<<<<<< HEAD
-    async getMeme() {
-            const response = await axios.get('https://meme-api.herokuapp.com/gimme');
-            this.setState({ memes: response.data });
-            console.log(response.data.url);
-        }
-=======
 
     componentWillUnmount() {
         this.abortController.abort();
     }
 
     async getMeme (){
-            const response = await axios.get('https://api.imgflip.com/get_memes', { signal: this.abortController.signal });
+            const response = await axios.get('https://meme-api.herokuapp.com/gimme', { signal: this.abortController.signal });
             if (this.state.isMounted) {
                 this.setState({ meme: response.data});
             }
         console.log(this.state.meme);
     }
->>>>>>> textarea
     render() {
             return(
                 <div>
-                    <img src={this.state.memes.url}/>
+                    <img src={this.state.meme.url}/>
                 </div>
             );
         }
