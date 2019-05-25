c1e1d4dcd82fb7d3ae8440bada112070843f130b
Maciej Korolik <maciej.korolik@gmail.com>
2019-05-25 06:26:31 +0200

    styles updated

--- client/src/MemeGenerator.js
+++ client/src/MemeGenerator.js
@@ -1,40 +1,44 @@
-import React from 'react';
-import axios from 'axios';
-import { Button } from 'antd';
+import React from "react";
+import axios from "axios";
+import { Button } from "antd";
+import "antd/dist/antd.css";
+import "./MemeGenerator.css";
 
-class MemeGenerator extends React.Component{
-    state = {
-        memes: []
-    };
+class MemeGenerator extends React.Component {
+  state = {
+    memes: []
+  };
 
-    componentDidMount(){
-        this.getMeme();
-    }
-    async getMeme() {
-            const response = await axios.get('https://meme-api.herokuapp.com/gimme');
-            this.setState({ memes: response.data });
-            console.log(response.data.url);
+  componentDidMount() {
+    this.getMeme();
+  }
+  async getMeme() {
+    const response = await axios.get("https://meme-api.herokuapp.com/gimme");
+    this.setState({ memes: response.data });
+    console.log(response.data.url);
+  }
 
-        }
+  async clicked() {
+    const response = await axios.get("https://meme-api.herokuapp.com/gimme");
+    this.setState({ memes: response.data });
+    console.log(this.state.memes.url);
+  }
 
-      async clicked() {
-            const response = await axios.get('https://meme-api.herokuapp.com/gimme');
-            this.setState({ memes: response.data });
-            console.log(this.state.memes.url)
-        }  
+  render() {
+    return (
+      <div className="meme-wrapper">
+        <img className="meme-image" src={this.state.memes.url} alt="meme" />
 
-    render() {
-            return(
-                <div>
-                    <div>
-                        <img style={{maxWidth: 500, maxHeight:500 }} src={this.state.memes.url} />
-                    </div>
-                    <div>
-                       <Button type = "primary" size = "large" onClick = {(e) => this.clicked()}>Give me next!</Button>
-                    </div>
-                </div>
-            );
-        }
-
+        <Button
+          className="meme-button"
+          type="primary"
+          size="large"
+          onClick={e => this.clicked()}
+        >
+          Give me next!
+        </Button>
+      </div>
+    );
+  }
 }
 export default MemeGenerator;
