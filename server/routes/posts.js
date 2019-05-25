288876f78c28f295253b4ea87c016843045f2589
Pawel Zaic <pawelzaic@gmail.com>
2019-05-25 04:51:20 +0200

    Added likes funcionality

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -9,7 +9,7 @@ posts.get("/all/", async (req, res) => {
   const posts = await Post.find();
   //console.log(posts);
   posts.forEach(post => {
-      return (post.likesCount = post.likesCountArray.length);
+    return (post.likesCount = post.likesCountArray.length);
   });
   //console.log(posts);
   res.send(posts);
@@ -21,7 +21,7 @@ posts.get("/", async (req, res) => {
     dateCurrent.getTime() - 7 * 24 * 60 * 60 * 1000
   );
   //console.log(dateCurrent);
- // console.log(dateWeekPast);
+  // console.log(dateWeekPast);
 
   const posts = await Post.find({
     date: {
@@ -62,7 +62,7 @@ posts.put("/", async (req, res) => {
     },
     {
       text: req.body.text,
-      attachment: req.body.attachment,
+      attachment: req.body.attachment
     },
     { new: true, omitUndefined: true }
   );
@@ -94,5 +94,40 @@ posts.delete("/all/", async (req, res) => {
   const result = await Post.deleteMany();
   res.send(result);
 });
+
+posts.put("/like/", async (req, res) => {
+  const post = await Post.findOne({
+    user: req.body.user,
+    date: req.body.date
+  });
+
+  //console.log(post);
+  //console.log(post.likesCountArray.length);
+  if (post.likesCountArray.length > 0) {
+    validateLike(post, req.body.likeGiver);
+  } else {
+    post.likesCountArray.push(req.body.likeGiver);
+    //console.log(`increased array: ${post.likesCountArray}`);
+  }
+
+  //console.log(post);
+
+  post.likesCount = post.likesCountArray.length;
+  //console.log(post);
+  postSent = await post.save();
+
+  res.send(postSent); 
+});
+
+function validateLike(post, likeGiver) {
+  if (post.likesCountArray.includes(likeGiver)) {
+    const index = post.likesCountArray.indexOf(likeGiver);
+    post.likesCountArray.splice(index, 1);
+    //console.log(`reduced array: ${post.likesCountArray}`);
+  } else {
+    post.likesCountArray.push(likeGiver);
+    //console.log(`increased array: ${post.likesCountArray}`);
+  }
+}
 
 module.exports = posts;
