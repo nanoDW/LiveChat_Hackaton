c55fada5fa30f04688786eb77c067b6e41e16d1c
Pawel Zaic <pawelzaic@gmail.com>
2019-05-25 06:22:10 +0200

    Added POST with combined data

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const LiveChatApi = require('livechatapi').LiveChatApi;
+const LiveChatApi = require("livechatapi").LiveChatApi;
 const db = require("./db/db")();
 const posts = require("./routes/posts");
 
 const API_PORT = 3001;
 
 const app = express();
-
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -21,15 +20,22 @@ app.use(function(req, res, next) {
 app.use(express.json());
 app.use("/api/posts", posts);
 
-
-
 // testowanie działania API
-const api = new LiveChatApi('kamila.spodymek@gmail.com', 'c73e145c22855a6775ad2424476c9b6f')
+const api = new LiveChatApi(
+  "kamila.spodymek@gmail.com",
+  "c73e145c22855a6775ad2424476c9b6f"
+);
 
-app.get('/agents', (req, res) => {
-    api.agents.list(function (data) {
-        res.send(data)
-    });
-})
+app.get("/agents", (req, res) => {
+  api.agents.list(function(data) {
+    res.send(data);
+  });
+});
+
+app.get("/agents/email=:email", (req, res) => {
+  api.agents.get(req.params.email, data => {
+    res.send(data);
+  });
+});
 
 app.listen(API_PORT, () => console.log(`Listening to port ${API_PORT}...`));
