74e0146a8fd9414d7a666bf1db4f4bfe29de2d8e
nanoDW <s.fluder@interia.eu>
2019-05-25 04:45:36 +0200

    Add textarea

--- client/src/MemeGenerator.js
+++ client/src/MemeGenerator.js
@@ -2,15 +2,27 @@ import React from 'react';
 import axios from 'axios';
 
 class MemeGenerator extends React.Component{
-    state = {meme: []};
+    state = {
+        meme: [],
+        isMounted: false
+    };
+
+    abortController = new AbortController();
 
     componentDidMount(){
+        this.setState({ isMounted: true});
         this.getMeme();
     }
 
+    componentWillUnmount() {
+        this.abortController.abort();
+    }
+
     async getMeme (){
-        const response = await axios.get('https://api.imgflip.com/get_memes');
-        this.setState({ meme: response.data});
+            const response = await axios.get('https://api.imgflip.com/get_memes', { signal: this.abortController.signal });
+            if (this.state.isMounted) {
+                this.setState({ meme: response.data});
+            }
         console.log(this.state.meme);
     }
     render() {
